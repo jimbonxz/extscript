2c0441c76a949c26f66a52baa63dd79a1b741a2f
jimbonxz <jimmy.merdyanto@gmail.com>
2016-12-09 15:54:32 +0700

    Update cekoutdetail.js

--- cekoutdetail.js
+++ cekoutdetail.js
@@ -132,7 +132,7 @@ else{getElementsByClassName=function(className,tag,elm){tag=tag||"*";elm=elm||do
 for(var l=0,ll=elements.length;l<ll;l+=1){current=elements[l];match=false;for(var m=0,ml=classesToCheck.length;m<ml;m+=1){match=classesToCheck[m].test(current.className);if(!match){break;}}
 if(match){returnElements.push(current);}}
 return returnElements;};}
-return getElementsByClassName(className,tag,elm);};String.prototype.reverse=function(){return this.split("").reverse().join("");};Number.prototype.withCommas=function(){var x=6,y=parseFloat(this).toFixed(2).toString().reverse();while(x<y.length){y=y.substring(0,x)+","+y.substring(x);x+=4;}return y.reverse();};Number.prototype.toCurrency=function(){return(arguments[0]?arguments[0]:"$")+this.withCommas();};function error(message){try{console.log(message);}catch(err){}}
+return getElementsByClassName(className,tag,elm);};String.prototype.reverse=function(){return this.split("").reverse().join("");};Number.prototype.withCommas=function(){var x=6,y=parseFloat(this).toFixed(2).toString().reverse();while(x<y.length){y=y.substring(0,x)+","+y.substring(x);x+=4;}return y.reverse();};Number.prototype.toCurrency=function(){return(arguments[0]?arguments[0]:"IDR")+this.withCommas();};function error(message){try{console.log(message);}catch(err){}}
 var simpleCart=new Cart();if(typeof jQuery!=='undefined')$(document).ready(function(){simpleCart.initialize();});else if(typeof Prototype!=='undefined')Event.observe(window,'load',function(){simpleCart.initialize();});else window.onload=simpleCart.initialize;$(document).ready(function(){function redirect(){window.location.assign('http://lihatpercobaan.blogspot.co.id');}
 function check(){if($('#mylicense').length===0){redirect();}
 else if($('#license').length===0){redirect();}
